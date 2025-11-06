fb5a1393777e7e7ffad819d1785ee7fb82d40a14
MrMblock <gabriel.saintlouis99@gmail.com>
2025-11-06 12:14:45 +0100

    responsive

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -3,15 +3,17 @@ import styles, { layout } from "../style";
 
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
-    <div className={layout.sectionImgReverse}>
-      <img src={bill} alt="billing" className="w-[80%] h-[85%] relative z-[5] mr-[120px]" />
-
+    <div className={`${layout.sectionImgReverse} flex justify-center sm:justify-start`}>
+      <img 
+        src={bill} 
+        alt="billing" 
+        className="w-[80%] h-[85%] relative z-[5] sm:mr-[120px]" 
+      />
       {/* gradient start */}
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
       <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
-
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
         Réserve tes cours <br className="sm:block hidden" /> en quelques clics
@@ -21,8 +23,7 @@ const Billing = () => (
         suivre ton planning et rester connecté avec la communauté du K. 
         Intensive Training Club.
       </p>
-
-      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+      <div className="flex flex-row flex-wrap justify-center sm:justify-start sm:mt-10 mt-6">
         <img src={apple} alt="app_store" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
         <img src={google} alt="play_store" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
       </div>
